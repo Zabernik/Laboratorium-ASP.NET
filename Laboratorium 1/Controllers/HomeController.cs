ca8c0f8dccae0690f175b2c41c5671748eb16319
Wiktor Pacewicz <wiktor.pacewicz@microsoft.wsei.edu.pl>
2023-10-04 15:48:07 +0200

    Up

--- Laboratorium 1/Controllers/HomeController.cs
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -4,6 +4,14 @@ using System.Diagnostics;
 
 namespace Laboratorium_1.Controllers
 {
+    public enum Operators
+    {
+        add,
+        sub,
+        mul,
+        div
+    }
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -20,6 +28,60 @@ namespace Laboratorium_1.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+        public IActionResult About(string author, int? id)
+        {
+            //string author = Request.Query["author"];
+            //string strid = Request.Query["id"];
+            //if (int.TryParse(strid, out var id))
+            //{
+            //    ViewBag.Author = $"{author} + id = {id}";
+            //}
+            //else
+            //{
+            //    ViewBag.Author = author;
+            //}
+            if (author is null || id is null)
+            {
+                return BadRequest();
+            }
+            ViewBag.Author = $"{author} id = {id}";
+            return View();
+        }
+        public IActionResult Calculator(string op, double dig1, double dig2)
+        {
+            double result = 0;
+            switch (op)
+            {
+                case "add":
+                    result = dig1 + dig2;
+                    break;
+                case "sub":
+                    result = dig1 - dig2;
+                    break;
+                case "mul":
+                    result = dig1 * dig2;
+                    break;
+                case "div":
+                    result = dig1 / dig2;
+                    break;
+            }
+            if (op is null || (dig1 == 0 && dig2 == 0))
+            {
+                ViewBag.Result = "Brak dzialania na kalkulatorze";
+            }
+            else
+            {
+                ViewBag.Result = $"Wynik dzialania {op} liczb {dig1} oraz {dig2} to {result}";
+            }
+
+            return View();
+        }
+
+        public IActionResult Kalkulator([FromQuery(Name = "operator")] Operators? op, double? x, double? y)
+        {
+            if(op == null || x == null || y == null)
             return View();
         }
 
