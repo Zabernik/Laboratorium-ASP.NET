45ebf3731056f34c2bcf288775f6e1b89f2162b4
Wiktor Pacewicz <wiktor.pacewicz@microsoft.wsei.edu.pl>
2023-10-18 15:48:42 +0200

    Up

---
+++ Laboratorium 3 - App/Controllers/ContactController.cs
@@ -0,0 +1,76 @@
+﻿using Laboratorium_3___App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___App.Controllers
+{
+    public class ContactController : Controller
+    {
+        // list of contacts
+        static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        static int id = 0;
+
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO add model to base or collection
+                model.ID = id++;
+                _contacts.Add(model.ID, model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[model.ID] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact model)
+        {
+            _contacts.Remove(model.ID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_contacts[id]);
+        }
+        [HttpPost]
+        public IActionResult Details() 
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Laboratorium 3 - App/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3___App.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Nie podałeś imienia!")]
+        [StringLength(maximumLength: 30, ErrorMessage = "Podane imię jest za długie!")]
+        public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Podany mail jest nieprawidłowy!")]
+        public string Email { get; set; }
+        [Phone(ErrorMessage = "Podany numer telefonu jest nieprawidłowy!")]
+        public string Phone { get; set; }
+        public DateTime? Birth { get; set; }
+    }
+}
